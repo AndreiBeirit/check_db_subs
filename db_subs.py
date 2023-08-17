024b1f6539544065228d51082c82130ebc3a9dc5
A.Beirit <76620440+SuFF-WW@users.noreply.github.com>
2023-08-17 18:06:49 +0500

    Update db_subs.py

--- db_subs.py
+++ db_subs.py
@@ -3,57 +3,65 @@ import os
 import requests
 import json
 
-user_db = os.environ["user_db"]
-password_db = os.environ["password_db"]
-host_db = os.environ["host_db"]
-port_db = 1992
-database_db = os.environ["database_db"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = "84.201.187.79"
+DB_PORT = 1992
+DB_DB = "accounts"
 WEBHOOK_URL = os.environ["SLACK_WEBHOOK"]
 
-# query = (
-#     "SELECT Email, EmailPassword, CreateDate FROM Accounts WHERE DATE(CreateDate) = DATE_SUB(CURDATE(), INTERVAL 30 DAY) AND `Status` IN (1,2,5,9,15,20,21)"
-# )
+db_config = {
+    'user': {DB_USER},
+    'password': {DB_PASSWORD},
+    'host': {DB_HOST},
+    'port': {DB_PORT},
+    'database': {DB_DB}
+}
 
-# connection = None
-# cursor = None
+query = (
+    "SELECT Email, EmailPassword, CreateDate FROM Accounts WHERE DATE(CreateDate) = DATE_SUB(CURDATE(), INTERVAL 30 DAY) AND `Status` IN (1,2,5,9,15,20,21)"
+)
 
-# try:
-#     connection = mysql.connector.connect(user=f"{user}", password=f"{password}", host=f"{host}", database=f"{database}", port=1992)
-#     cursor = connection.cursor()
-#     cursor.execute(query)
-#     results = cursor.fetchall()
+connection = None
+cursor = None
+
+try:
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
     
-#     with open('D:/sub_end.txt', 'w') as file:
-#         for row in results:
-#             email, password, create_date = row
-#             file.write(f"{email}:{password} - {create_date}\n")
+    with open('D:/sub_end.txt', 'w') as file:
+        for row in results:
+            email, password, create_date = row
+            file.write(f"{email}:{password} - {create_date}\n")
     
-#     with open("D:/sub_end.txt", "r") as f:
-#         file_contents = f.read()
+    with open("D:/sub_end.txt", "r") as f:
+        file_contents = f.read()
 
-#     if file_contents.strip():
-#         message = f":police: Ending subscriptions detected! :police:\n{file_contents}"
-#     else:
-#         message = ":white_check_mark: All subscriptions is up to date! :white_check_mark:"
+    if file_contents.strip():
+        message = f":police: Ending subscriptions detected! :police:\n{file_contents}"
+    else:
+        message = ":white_check_mark: All subscriptions is up to date! :white_check_mark:"
 
-#     headers = {"Content-type": "application/json"}
-#     payload = {"text": message}
+    headers = {"Content-type": "application/json"}
+    payload = {"text": message}
 
-#     response = requests.post(f"{webhook_url}", data=json.dumps(payload), headers=headers)
+    response = requests.post(f"{webhook_url}", data=json.dumps(payload), headers=headers)
 
-#     if response.status_code != 200:
-#         raise ValueError(
-#             f"Request to Slack returned an error {response.status_code}, the response is:\n{response.text}"
-#         )
-#     else:
-#         print("Message sent successfully!")
+    if response.status_code != 200:
+        raise ValueError(
+            f"Request to Slack returned an error {response.status_code}, the response is:\n{response.text}"
+        )
+    else:
+        print("Message sent successfully!")
 
-# except mysql.connector.Error as err:
-#     print("Query error:", err)
+except mysql.connector.Error as err:
+    print("Query error:", err)
 
-# finally:
-#     if cursor is not None:
-#         cursor.close()
-#     if connection is not None:
-#         connection.close()
+finally:
+    if cursor is not None:
+        cursor.close()
+    if connection is not None:
+        connection.close()
 
